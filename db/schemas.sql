e3ace9e334a1c627d4fd16dd03fb9e687b617361
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-11 22:04:55 -0500

    clean scheqmas and created seeds

--- db/schemas.sql
+++ db/schemas.sql
@@ -14,7 +14,9 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER, 
     INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_department 
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) ON DELETE SET NULL
 );
 
 
@@ -23,8 +25,8 @@ CREATE TABLE employee (
     first_name  VARCHAR(30) NOT NULL, 
     last_name  VARCHAR(30) NOT NULL, 
     role_id INTEGER, 
-   CONSTRAINT fk_role FOREIGN KEY (role_id) 
-   REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) 
+    REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER,
     INDEX manager_ind (manager_id),
     CONSTRAINT fk_manager 
