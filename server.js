2abe67ff8956ceaa65c79d48afca51f7c0662686
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-12 21:36:52 -0500

    test host  127.0.0.1 mac error

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const cTable = require("console.table");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: '',
+    host: '127.0.0.1',
+    user: 'root',
     password: process.env.MYSQL_PASSWORD,
     database: 'employee_db'
   });
@@ -18,4 +18,6 @@ const connection = mysql.createConnection({
     console.log('connected as id ' + connection.threadId);
     afterConnection();
   });
+
+  
 
