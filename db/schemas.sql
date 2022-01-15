ab9c011df9feb3f3aef055f788fa6cc1ab4fa763
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-14 23:34:37 -0500

    Wip

--- db/schemas.sql
+++ db/schemas.sql
@@ -2,9 +2,8 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
-
 CREATE TABLE department (
-    id  INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
@@ -14,23 +13,18 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER, 
     INDEX dep_ind (department_id),
-    CONSTRAINT fk_department 
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
-
 
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name  VARCHAR(30) NOT NULL, 
-    last_name  VARCHAR(30) NOT NULL, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER, 
-    CONSTRAINT fk_role FOREIGN KEY (role_id) 
-    REFERENCES role(id) ON DELETE SET NULL,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER,
     INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager 
-    FOREIGN KEY (manager_id) 
-    REFERENCES employee(id) ON DELETE SET NULL
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,31 @@
+USE employee_db;
+
 INSERT INTO department (name)
 VALUES 
 ('Administration'),
 ('CSR'),
-('Art Deparment'),
-('IT'),
+('Art Department'),
+('IT');
 
-INSERT INTO role (title, salary,department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
 ('Manager', 120000, 1),
-(1, 'Admin Office', 100000, 1),
+('Admin Office', 100000, 1),
 ('Costumer Service 1', 40000, 2),
-'Costumer Service 2', 40000, 2),
+('Costumer_Service 2', 40000, 2),
 ('Graphics 1', 45000, 3),
 ('Graphics 2', 45000, 3),
 ('Software Enginner', 120000, 4),
-('Full Stack Web Developer', 85000, 5),
+('Full Stack Web Developer', 85000, 5);
 
-INSERT INTO employee (first_name, last_name,role_id, manager_id,)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ('Chuck', 'OB', 1, 1),
 ('Sue', 'P', 2, 1),
 ('Sue', 'OB', 3, NULL),
-('Rain', 'D', 4, NULL,),
-('Bernie', 'A', 5, NULL,),
- 'Jessica', 'K', 6, NULL,),
-('Kevin', 'S', 7, NULL,),
-('Asley', 'M', 4, NULL,),
+('Rain', 'D', 4, NULL),
+('Bernie', 'A', 5, NULL),
+('Jessica', 'K', 6, NULL),
+('Kevin', 'S', 7, NULL),
+('Asley', 'M', 4, NULL);
 
