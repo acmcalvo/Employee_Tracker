c2d51cbe982657517e2c86b1a79e208c87da29cb
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-15 23:58:20 -0500

    WIP

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,7 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Full Stack Developer', 80000, 1),
+('General Magager', 180000, 1),
 ('Software Engineer', 120000, 1),
 ('Accountant', 10000, 2), 
 ('Finanical Analyst', 150000, 2),
@@ -20,11 +20,11 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
-('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('Chuck', 'OBuckey', 2, null),
+('Susan', 'Pearce', 1, 1),
+('Sue', 'OBuckey', 4, null),
+('Charles', 'Thompson', 3, 3),
+('John', 'Hoffman', 6, null),
+('Shabon', 'Mcgregor', 5, 5),
+('Bernie', 'Argenal', 7, null),
+('Jessica', 'Keating', 8, 7);
