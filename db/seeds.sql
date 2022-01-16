b2ba70eb895a00f55fb8e2ed21f510228c30fbcf
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-15 22:38:03 -0500

    WIP

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,30 @@
-USE employee_db;
 
 INSERT INTO department (name)
 VALUES 
-('Administration'),
-('CSR'),
-('Art Department'),
-('IT');
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Manager', 120000, 1),
-('Admin Office', 100000, 1),
-('Costumer Service 1', 40000, 2),
-('Costumer_Service 2', 40000, 2),
-('Graphics 1', 45000, 3),
-('Graphics 2', 45000, 3),
-('Software Enginner', 120000, 4),
-('Full Stack Web Developer', 85000, 5);
+('Full Stack Developer', 80000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Chuck', 'OB', 1, 1),
-('Sue', 'P', 2, 1),
-('Sue', 'OB', 3, NULL),
-('Rain', 'D', 4, NULL),
-('Bernie', 'A', 5, NULL),
-('Jessica', 'K', 6, NULL),
-('Kevin', 'S', 7, NULL),
-('Asley', 'M', 4, NULL);
-
+('Mark', 'Miller', 2, null),
+('Devin', 'Anderson', 1, 1),
+('Mary', 'Brown', 4, null),
+('Ashley', 'Jones', 3, 3),
+('Tyler', 'Moore', 6, null),
+('Ana', 'Sanchez', 5, 5),
+('Lewis', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
